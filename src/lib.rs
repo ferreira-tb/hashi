3e363fbdee9d866385cc37cfb60f071786ab50f5
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-16 16:42:14 -0300

    feat: add `Window::fetch`

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,17 @@ pub mod timer;
 pub use prelude::*;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, Response};
 
 pub fn window() -> web_sys::Window {
   web_sys::window().unwrap_throw()
+}
+
+pub async fn fetch(request: &Request) -> Result<Response, JsValue> {
+  let promise = window().fetch_with_request(&request);
+  let response = JsFuture::from(promise).await?;
+  Ok(response.dyn_into().unwrap_throw())
 }
 
 pub fn stop() {
