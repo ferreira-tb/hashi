61b9749bfa457d5d0c053a3672610d01ca70801c
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-16 21:03:16 -0300

    feat: `query_all_in`

--- src/dom.rs
+++ src/dom.rs
@@ -2,6 +2,7 @@ use crate::timer::sleep;
 use js_sys::Date;
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
+use web_sys::{Element, NodeList};
 
 pub fn document() -> web_sys::Document {
   super::window().document().unwrap_throw()
@@ -15,22 +16,40 @@ pub fn body() -> web_sys::HtmlElement {
   document().body().unwrap_throw()
 }
 
-pub fn document_element() -> web_sys::Element {
+pub fn document_element() -> Element {
   document().document_element().unwrap_throw()
 }
 
-pub fn query(selector: &str) -> Option<web_sys::Element> {
+pub fn query(selector: &str) -> Option<Element> {
   document()
     .query_selector(selector)
     .unwrap_throw()
 }
 
-pub fn query_all(selector: &str) -> Vec<web_sys::Element> {
+pub fn query_in(element: Element, selector: &str) -> Option<Element> {
+  element
+    .query_selector(selector)
+    .unwrap_throw()
+}
+
+pub fn query_all(selector: &str) -> Vec<Element> {
   let list = document()
     .query_selector_all(selector)
     .unwrap_throw();
 
-  js_sys::try_iter(&list)
+  collect_node_list(&list)
+}
+
+pub fn query_all_in(element: Element, selector: &str) -> Vec<Element> {
+  let list = element
+    .query_selector_all(selector)
+    .unwrap_throw();
+
+  collect_node_list(&list)
+}
+
+fn collect_node_list(list: &NodeList) -> Vec<Element> {
+  js_sys::try_iter(list)
     .unwrap_throw()
     .unwrap_throw()
     .map(|value| value.map(JsCast::unchecked_into))
@@ -38,7 +57,7 @@ pub fn query_all(selector: &str) -> Vec<web_sys::Element> {
     .unwrap_throw()
 }
 
-pub async fn wait_element(selector: &str, secs: u32) -> Option<web_sys::Element> {
+pub async fn wait_element(selector: &str, secs: u32) -> Option<Element> {
   let interval = Duration::from_millis(100);
   let timeout = Date::now() + (f64::from(secs) * 1000.0);
 
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,3 @@
-pub use crate::dom::{document, query, query_all, wait_element};
+pub use crate::dom::{document, query, query_all, query_all_in, query_in, wait_element};
 pub use crate::location::{hostname, href, reload, set_href};
 pub use crate::timer::sleep;
